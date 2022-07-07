6edab57c8dc442f9e6ed9889ed7bf22ee4e77533
wesmith <smiwarsky@gmail.com>
2022-07-07 02:31:36 -0700

    simple vanilla-python testing working

--- operator_test.py
+++ operator_test.py
@@ -23,40 +23,20 @@ SOFTWARE.
 '''
 
 import operator_utils as ws
-import unittest
 
-aa = ws.Coords()
-data = aa.spherical
+aa = ws.coord_xforms()
 
-class MyTest(unittest.TestCase):
-    
-    def test_for_zero(self):
-        dd  = ws.Operators(data)
-        val = dd.divergence(dd.curl())
-        vec = dd.curl(dd.gradient())
-        print(val, vec)
-        with self.subTest():
-            self.assertEqual(val, 0, "should be zero")
-        with self.subTest():
-            self.assertEqual(vec._eval_is_zero_matrix(), True, "should be zero vector")
-    '''
-    def test_cylindrical(self):
-        dd = ws.Operators(aa.cylindrical)
-        self.assertEqual(dd.divergence(dd.curl()), 0, "should be zero")
-    def test_cartesian(self):
-        dd = ws.Operators(aa.cartesian)
-        self.assertEqual(dd.divergence(dd.curl()), 0, "should be zero")        
-    '''
-'''
-class Multiple(MyTest):
-    
-    def __init__(self, aa):
-        self.aa = aa
+def test_for_zero(data):
+    dd = ws.Operators(data)
+    assert dd.divergence(dd.curl()) == 0, 'div(curl) test failed by {}'.format(dd.name)
+    assert dd.curl(dd.gradient())._eval_is_zero_matrix(), 'curl(grad) test failed by {}'.format(dd.name)
+    print('"div(curl) and curl(grad)" tests passed by: {}'.format(dd.name))
 
-    def test_over_coords(self):
-        self.test_for_zero(self.aa.spherical)
-        self.test_for_zero(self.aa.cylindrical)
-        self.test_for_zero(self.aa.cartesian)
-'''
+def main():
 
-unittest.main()
+    for key, val in aa.items():
+        test_for_zero(val)
+
+if __name__ == '__main__':
+
+    main()
